36d673276e93d38c2e5a73ee3a214faf278c6b4d
seemabhatta <seema.bhatta@gmail.Com>
2025-08-25 14:03:21 -0400

    postgres removed

--- server/database.py
+++ server/database.py
@@ -7,7 +7,7 @@ from loguru import logger
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./loan_boarding.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
